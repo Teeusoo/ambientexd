b0ef936821c00bc4d6620ecbbf4bba7429b02253
Matheus <matheus02@msn.com>
2018-06-11 10:49:33 -0300

    Comecando a desenhar os tiles

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
